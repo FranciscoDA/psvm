00e69e442c870196794e8840a4514f6dcb913afb
FranciscoDA <franciscoda@outlook.com>
2017-09-05 16:41:54 -0300

    serachgrid_casero: output to file

--- cpp/searchgrid_casero.sh
+++ cpp/searchgrid_casero.sh
@@ -7,15 +7,15 @@ do
 	do
 		for gamma in 0.25 0.5 0.75 1
 		do
-			./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel rbf --gamma $gamma
+			./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel rbf --gamma $gamma > "results/C_${C}_tn_${tn}_rbf_gamma_${gamma}.txt"
 		done
 		for p in 2 4 6 9
 		do
 			for c in 0 1
 			do
-				./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel poly --power $p --constant $c
+				./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel poly --power $p --constant $c > "results/C_${C}_tn_${tn}_poly_pow_${p}_c_${c}.txt"
 			done
 		done
-		./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel linear
+		./cusvm --train-attributes mnist/train-images.idx3-ubyte --train-labels mnist/train-labels.idx1-ubyte --train-format idx --train-n 15000 --test-attributes mnist/t10k-images.idx3-ubyte --test-labels mnist/t10k-labels.idx1-ubyte --test-format idx --test-n $tn --normalize-zero-one --cost $C --kernel linear > "results/C_${C}_tn_${tn}_linear.txt"
 	done
 done
