81ea3b61b8747554b7e5385cc5c54f84cadeb021
FranciscoDA <franciscoda@outlook.com>
2017-09-14 04:19:41 -0300

    dont ignore get.sh

---
+++ mnist/get.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILENAMES="train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ubyte t10k-labels-idx1-ubyte"
+
+for fn in $FILENAMES
+do
+	wget http://yann.lecun.com/exdb/mnist/$fn.gz
+	gunzip -c $fn.gz > $fn
+done
